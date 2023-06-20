66945d5924da4240b9393a00595cc360a7ee83fd
rayssasilv4 <rayssacamila@outlook.com>
2023-06-20 10:37:51 -0300

    Exercicio Variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+        var msg = "Página carregada com sucesso!";
+        alert(msg);
+        
+        var x = 4;
+        var y = 2;
+
+        var z = x + y;
+        document.getElementById("soma").innerHTML = z;
+
+        var z = x / y;
+        document.getElementById("divisao").innerHTML = z;
+
+        var z = x * y;
+        document.getElementById("multi").innerHTML = z;
+
+        var z = x < y;
+        document.getElementById("comparacao").innerHTML = z;
+
